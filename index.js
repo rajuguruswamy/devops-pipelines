e1f3fc28da8d1ac7fe09529ad4babf527499d302
rajuguruswamy <raju.guruswamy.sg@gmail.com>
2026-02-21 16:46:55 +0800

    added cicd pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello, DevOps World!');
+console.log('I am learning CI/CD using GitHub Actions.');
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('Starting tests... ');
+setTimeout(() => {
+  console.log('waiting three seconds...');
+}, 3000);
+
+console.log('tests completed!');
